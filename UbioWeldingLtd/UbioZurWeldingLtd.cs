3f92b2fc5e54121176cd794050e5314efd8c7eff
Alewx <Alewxx@googlemail.com>
2014-09-29 13:56:42 +0200

    Module Manager Integration 1.0
    -Integration of Database reload with ModuleManager (limited to
    referenced version (2.3.5))
    -check for ModuleManager inside gamedata folder
    -switch for stock reloading and MM reloading depending on installed
    ModuleManager

--- UbioWeldingLtd/UbioZurWeldingLtd.cs
+++ UbioWeldingLtd/UbioZurWeldingLtd.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 
 namespace UbioWeldingLtd
@@ -16,7 +17,6 @@ namespace UbioWeldingLtd
 			infoWindow,
 			savedWindow,
 			overwriteDial
-
 		}
 
 		public static UbioZurWeldingLtd instance { get; private set; }
@@ -51,6 +51,8 @@ namespace UbioWeldingLtd
 		 */
 		public void Awake()
 		{
+			Debug.Log(string.Format("{0}{1}", Constants.logPrefix, Constants.logVersion));
+
 			instance = this;
 			initConfig();
 			_state = DisplayState.none;
@@ -85,6 +87,7 @@ namespace UbioWeldingLtd
 			}
 			//_stockToolbarButton.SetFalse();
 		}
+
 		/// <summary>
 		/// Loads the config for the Welding or prepares default values and generates a new config
 		/// </summary>
@@ -519,20 +522,15 @@ namespace UbioWeldingLtd
 
 			if (_config.dataBaseAutoReload)
 			{
-				ReloadDatabase();
+				if (WeldingHelpers.isModuleManagerInstalled())
+				{
+					DatabaseHandler.ReloadDatabase();
+				}
+				else
+				{
+					StartCoroutine(DatabaseHandler.DatabaseReloadWithMM());
+				}
 			}
-		}
-
-		/*
-		 * Reload the Database
-		 */
-		private void ReloadDatabase()
-		{
-			//reload database Big thanks to AncientGammoner (KSP Forum)
-			GameDatabase.Instance.Recompile = true;
-			GameDatabase.Instance.StartLoad();
-			PartLoader.Instance.Recompile = true;
-			PartLoader.Instance.StartLoad();
 		}
 
 		/*
