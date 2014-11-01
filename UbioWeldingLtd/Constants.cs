07d89e76d3e04a897b7d416d081cb2685cc40b71
Girka2k <girka@mail.ru>
2014-11-01 18:57:56 +0300

    version attributes update

--- UbioWeldingLtd/Constants.cs
+++ UbioWeldingLtd/Constants.cs
@@ -11,7 +11,7 @@ namespace UbioWeldingLtd
 	static class Constants
 	{
 		//Logs/debug constants
-		public const string logVersion		  = "v2.0pt5-0.25.0-Unofficial";
+		public const string logVersion		  = "v2.0pt5-0.24.2/0.25.0-Unofficial-v.03";
 		public const string logWarning		  = "WARNING ";
 		public const string logError			= "ERROR ";
 		public const string logPrefix		   = "[WeldingTool] ";
