ae352626a4ba82033190015c9f9e77ba098511ac
Lisias T <me@lisias.net>
2018-04-10 00:47:33 -0300

    Bumbing version and updating changelog.

--- UbioWeldingLtd/Version.cs
+++ UbioWeldingLtd/Version.cs
@@ -5,8 +5,8 @@
 		public const int major = 2;
 		public const int minor = 6;
 		public const int patch = 0;
-		public const int build = 1;
-		public const string Number = "2.6.0.1";
+		public const int build = 3;
+		public const string Number = "2.6.0.3";
 		public const string Text = Number + " Alpha";
 	}
 }
