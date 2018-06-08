268917b64d768361f50fb6974aa8f60b9ac10c0b
Lisias T <me@lisias.net>
2018-06-07 23:41:55 -0300

    Adding scripts for automation

---
+++ pack.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+VERSION=$(cat ./UbioWeldingLtd/Version.cs | grep -Po 'public const string Number = "(.+)";' | sed 's/public const string Number = "//' | sed 's/";//')
+echo $VERSION
+FILE=UbioWeldContinuum-$VERSION.zip
+rm $FILE
+zip -r $FILE ./GameData/* -x .DS_Store
