3c4c1f4f08c99052333d95d47291bc1e5cbc3089
Lisias T <me@lisias.net>
2018-08-03 15:16:09 -0300

    Updating the automation scripts.

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-source ../CONFIG.inc
+source ./CONFIG.inc
 
-VERSIONFILE=UbioWeldContinuum.version
+VERSIONFILE=$PACKAGE.version
 
 scp -i $SSH_ID ./GameData/net.lisias.ksp/$VERSIONFILE $SITE:/$TARGETPATH
